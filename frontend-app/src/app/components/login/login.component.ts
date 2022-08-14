f2e7c6b5195642fae9e1c4a775288ce995658c6e
V3795083 <eduard.lata@ness.com>
2022-08-14 21:50:51 +0300

    added the login page

---
+++ frontend-app/src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  hide = true;
+
+  onDigitInput(event: any){
+    let element;
+    if (event.code !== 'Backspace')
+      element = event.srcElement.nextElementSibling;
+
+    if (event.code === 'Backspace')
+      element = event.srcElement.previousElementSibling;
+
+    if(element == null)
+      return;
+    else
+      element.focus();
+  }
+}
